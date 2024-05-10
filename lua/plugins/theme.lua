948712fc440abad57762d46ee561bdcccf9084ff
Isaiah Banks <isaiah.banks42@gmail.com>
2024-05-10 17:04:45 -0400

    dashboard condom head commit v2

---
+++ lua/plugins/theme.lua
@@ -0,0 +1,20 @@
+return {
+	{
+		"Mofiqul/vscode.nvim",
+	},
+	{
+		"nyoom-engineering/oxocarbon.nvim",
+	},
+	{
+		"eldritch-theme/eldritch.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {},
+	},
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "eldritch",
+		},
+	},
+}
