060c9d22ba585ad8040506970da5573f43434d84
Isaiah Banks <isaiah.banks42@gmail.com>
2024-05-10 17:04:28 -0400

    dashboard condom head

--- lua/plugins/biome.lua
+++ lua/plugins/biome.lua
@@ -1,40 +1,9 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      --- other options
-      servers = {
-        tsserver = {
-          on_attach = function(client)
-            -- this is important, otherwise tsserver will format ts/js
-            -- files which we *really* don't want.
-            client.server_capabilities.documentFormattingProvider = false
-          end,
-        },
-        biome = {},
-        -- other language servers
-      },
+  "stevearc/conform.nvim",
+  opts = {
+    -- Use a sub-list to run only the first available formatter
+    formatters_by_ft = {
+      javascript = { { "biome" } },
     },
-  },
-  {
-    "nvimtools/none-ls.nvim",
-    opts = function(_, opts)
-      local nls = require("null-ls").builtins
-      opts.sources = vim.list_extend(opts.sources or {}, {
-        nls.formatting.biome,
-
-        -- or if you like to live dangerously like me:
-        nls.formatting.biome.with({
-          args = {
-            "check",
-            "--apply-unsafe",
-            "--formatter-enabled=true",
-            "--organize-imports-enabled=true",
-            "--skip-errors",
-            "$FILENAME",
-          },
-        }),
-      })
-    end,
   },
 }
